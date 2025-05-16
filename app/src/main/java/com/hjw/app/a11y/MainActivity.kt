3258bcdb060361e21fbf3ad02669be0d5f6f2c6a
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2025-05-16 20:48:32 +0900

    ✨ 텍스트 표현 제어

--- app/src/main/java/com/hjw/app/a11y/MainActivity.kt
+++ app/src/main/java/com/hjw/app/a11y/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.hjw.app.a11y
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.text
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hjw.app.a11y.ui.theme.A11YTheme
@@ -32,13 +36,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             A11YTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
+                    Log.d("fog", "onCreate: innerPadding = $innerPadding")
+
+                    val contentPadding = PaddingValues(
+                        start = 16.dp,
+                        top = innerPadding.calculateTopPadding(),
+                        end = 16.dp,
+                        bottom = innerPadding.calculateBottomPadding()
+                    )
+
+                    Column(modifier = Modifier.padding(contentPadding)) {
                         BasicA11y(name = "A11J", contentDescription = "내 프로필")
+                        Spacer(modifier = Modifier.size(16.dp))
                         Profile(name = "나는 개발자", role = "안드로이드 개발자", contentDescription = "내 프로필")
+                        Spacer(modifier = Modifier.size(16.dp))
                         SwipeToDismissDemo(
                             item = "todo",
                             onDelete = { }
                         )
+                        Spacer(modifier = Modifier.size(16.dp))
+                        ControllingTextRepresentation()
                     }
                 }
             }
@@ -49,13 +66,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicA11y(
     name: String,
-    modifier: Modifier = Modifier,
     contentDescription: String = ""
 ) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
-            .padding(16.dp)
+        modifier = Modifier
             .semantics {
                 heading()
                 this.contentDescription = contentDescription // 기본 접근성 설정.
@@ -93,6 +108,17 @@ fun Profile(
             Text(text = role, modifier = Modifier)
         }
     }
+}
+
+@Composable
+fun ControllingTextRepresentation() {
+    Text(
+        text = "₩1,234,567",
+        modifier = Modifier.semantics {
+            // 스크린리더가 읽을 대체 텍스트 제공
+            text = AnnotatedString("1,234,567원")
+        }
+    )
 }
 
 @Preview(showBackground = true)
